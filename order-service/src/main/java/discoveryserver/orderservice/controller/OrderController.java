9b87aa89218761141e648bb2b6c6c27595716f9c
SaraaMiss <sara.kaouar.christol@gmail.com>
2023-10-26 16:04:11 +0200

    circuit breaker

--- order-service/src/main/java/discoveryserver/orderservice/controller/OrderController.java
+++ order-service/src/main/java/discoveryserver/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package discoveryserver.orderservice.controller;
 
 import discoveryserver.orderservice.service.OrderService;
 import discoveryserver.orderservice.dto.OrderRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +15,17 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="inventory",fallbackMethod = "fallbackMethod")
     public String PlaceOrder(@RequestBody OrderRequest orderRequest) throws IllegalAccessException {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+
+
+    public String fallbackMethod(OrderRequest orderRequest,RuntimeException runtimeException ){
+
+        return "Oops! Something went wrong,please order after some time !";
     }
 
 }
