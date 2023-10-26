d15a7052613e273403690b6a5d552263f5f397c5
SaraaMiss <sara.kaouar.christol@gmail.com>
2023-10-26 10:50:53 +0200

    security

--- discovery-server/src/main/java/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/discoveryserver/config/SecurityConfig.java
@@ -1,37 +1,25 @@
 package discoveryserver.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguerAdapter {
-    @Value("${eureka.username}")
-    private String username;
-    @Value("${eureka.password}")
-    private String password;
-    @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder ) throws Exception {
+public class SecurityConfig {
 
 
-        authenticationManagerBuilder.inMemoryAuthentication()
-                .withUser(username).password(password)
-                .authorities("USER");
-    }
 
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
-                .authorizeHttpRequests().anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
+        return httpSecurity.build();
     }
 }
